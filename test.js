eb0da195f48226a6b41daaba7fa88ce4077b7296
Felipe Álvarez <falvarez@ciudadanointeligente.org>
2017-03-28 10:34:30 -0300

    Removing trailing spaces

--- test.js
+++ test.js
@@ -18,7 +18,7 @@ test('rut', async t => {
     rut,
     dv
   } = (await rutificador({ rut: '12585741-8' }))[0]
-  
+
   t.is(name, 'VIÑUELA INFANTE JOSE MIGUEL')
   t.is(rut, 12585741)
   t.is(dv, '8')
